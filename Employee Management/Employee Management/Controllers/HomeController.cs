4e84a580fc227f907a7ddeb3f3d8006e68b8af75
chonadhvarma <plpsatish456@gmail.com>
2020-05-14 12:04:02 +0530

    added mvc controller

---
+++ Employee Management/Employee Management/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Management.Controllers
+{
+    public class HomeController:Controller
+    {
+        public string Index()
+        {
+            return " Varma";
+        }
+    }
+}
--- Employee Management/Employee Management/Startup.cs
+++ Employee Management/Employee Management/Startup.cs
@@ -17,6 +17,7 @@ namespace Employee_Management
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,11 +33,7 @@ namespace Employee_Management
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.Map("/", async context =>
-               {
-                   await context.Response.WriteAsync("Hello World!");
-
-               });
+                endpoints.MapDefaultControllerRoute();
 
             });
            
